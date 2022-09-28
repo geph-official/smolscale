ade4bd5f1dc942b81aa87353115411047a763966
nullchinchilla <nullchinchilla@pm.me>
2022-09-28 17:48:17 -0500

    fix

--- src/fastcounter.rs
+++ src/fastcounter.rs
@@ -6,31 +6,24 @@ use thread_local::ThreadLocal;
 /// A write-mostly, read-rarely counter
 #[derive(Default, Debug)]
 pub struct FastCounter {
-    counters: ThreadLocal<CachePadded<AtomicUsize>>,
+    counter: AtomicUsize,
 }
 
 impl FastCounter {
     /// Increment the counter
     #[inline]
     pub fn incr(&self) {
-        self.counters
-            .get_or_default()
-            .fetch_add(1, Ordering::Relaxed);
+        self.counter.fetch_add(1, Ordering::Relaxed);
     }
 
     /// Decrement the counter
     #[inline]
     pub fn decr(&self) {
-        self.counters
-            .get_or_default()
-            .fetch_sub(1, Ordering::Relaxed);
+        self.counter.fetch_sub(1, Ordering::Relaxed);
     }
 
     /// Get the total count
     pub fn count(&self) -> usize {
-        self.counters
-            .iter()
-            .map(|f| f.load(Ordering::Relaxed))
-            .fold(0usize, |x, y| x.wrapping_add(y))
+        self.counter.load(Ordering::Relaxed)
     }
 }
