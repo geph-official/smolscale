7e51177efa4ed47bbe0146e68d604330fc92a382
nullchinchilla <nullchinchilla@pm.me>
2023-11-06 10:47:14 -0500

    Add async-compat library and use CompatExt for compatibility in monitor_loop function

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@
 //! - **When the CPU cores are not fully loaded**: Traditional work stealing optimizes for the case where most workers have work to do, which is only the case in fully-loaded scenarios. When workers often wake up and go back to sleep, however, a lot of CPU time is wasted stealing work. `smolscale` will instead drastically reduce CPU usage in these circumstances --- a `async-executor` app that takes 80% of CPU time may now take only 20%. Although this does not improve fully-loaded throughput, it significantly reduces power consumption and does increase throughput in circumstances where multiple thread pools compete for CPU time.
 //! - **When a lot of message-passing is happening**: Message-passing workloads often involve tasks quickly waking up and going back to sleep. In a work-stealing scheduler, this again floods the scheduler with stealing requests. `smolscale` can significantly improve throughput, especially compared to executors like `async-executor` that do not special-case message passing.
 
+use async_compat::CompatExt;
 use backtrace::Backtrace;
 use dashmap::DashMap;
 use fastcounter::FastCounter;
@@ -100,7 +101,8 @@ fn monitor_loop() {
                     } else {
                         new_executor::run_local_queue().await;
                     };
-                };
+                }
+                .compat();
                 if process_io {
                     async_io::block_on(future)
                 } else {
